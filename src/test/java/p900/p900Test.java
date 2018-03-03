73034d1120311dfe4702231df442a475f2ccf18a
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-03-03 21:39:04 +0200

    add SUREFIRE in POM

--- src/test/java/p900/p900Test.java
+++ src/test/java/p900/p900Test.java
@@ -5,6 +5,11 @@ import org.testng.annotations.Test;
 public class p900Test extends WebDriverTestBase{
     @Test
     public void openPage(){
-        driver.get("p900.com.ua");
+        driver.get("http://p900.com.ua");
+        try {
+            Thread.sleep(50000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
