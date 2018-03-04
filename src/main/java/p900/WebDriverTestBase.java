11d1f4381c0c9b58042d56d9a1f8bdc428ac142d
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-03-04 15:59:09 +0200

    add HashMap for opera caps

--- src/main/java/p900/WebDriverTestBase.java
+++ src/main/java/p900/WebDriverTestBase.java
@@ -1,8 +1,6 @@
 package p900;
 
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.opera.OperaDriver;
-import org.openqa.selenium.opera.OperaOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
@@ -11,12 +9,8 @@ import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.net.URI;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.openqa.selenium.remote.DesiredCapabilities.operaBlink;
-import static org.slf4j.MDC.put;
 
 public class WebDriverTestBase {
     protected RemoteWebDriver driver;
@@ -24,7 +18,6 @@ public class WebDriverTestBase {
     @Parameters({"browserName", "browserVersion"})
     @BeforeClass
     public void setUp(@Optional String browserName, @Optional String browserVersion) throws Exception{
-        browserName="opera";
         DesiredCapabilities browser = new DesiredCapabilities();
             browser.setBrowserName(browserName);
             browser.setVersion(browserVersion);
