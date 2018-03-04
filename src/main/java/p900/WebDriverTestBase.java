d00f84d5a72d03b48eb6b83cf7e87dc116e5d905
melnichenko.andrey <melnichenko.andrey@gmail.com>
2018-03-04 12:45:11 +0200

    set new options for opera capabilities

--- src/main/java/p900/WebDriverTestBase.java
+++ src/main/java/p900/WebDriverTestBase.java
@@ -21,7 +21,7 @@ public class WebDriverTestBase {
         browser.setBrowserName(browserName);
         browser.setVersion(browserVersion);
         if (browserName.equals("opera")){
-            browser.setCapability("operaOptions:binary", "/usr/bin/opera");
+            browser.setCapability("opera.binary", "/usr/bin/opera");
         }
         browser.setCapability("enableVNC", true);
         driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(),browser);
